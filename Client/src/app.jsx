687a262d292a541262a66d8cd81b4e4f791d53f9
Eva Despinoy <despinoy@kth.se>
2021-09-22 14:31:10 +0200

    add working cocktailDB API call, and basic search page

--- Client/src/app.jsx
+++ Client/src/app.jsx
@@ -1,9 +1,15 @@
-import React from 'react'; 
-import ReactDOM from 'react-dom'; 
+import React from "react";
+import ReactDOM from "react-dom";
+import { AddBeverageToMenuPresenter } from "./components/addBeverageToMenuPresenter";
 
 const App = () => {
-    console.log('Running app');
-    return <div>Hello Meta</div>
+  console.log("Running app");
+  return (
+    <div>
+      <div>Hello Meta</div>
+      <AddBeverageToMenuPresenter />
+    </div>
+  );
 };
 
-ReactDOM.render(<App/>, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
