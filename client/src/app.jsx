5fbf0f97971a1985667250f4e2b82450f043a846
Amalia <aberglof@kth.se>
2021-09-26 23:33:56 +0200

    added new component

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
+import UserSignIn from './components/UserSignIn'
 
 const App = () => {
     console.log('Running app');
-    return <div>Hello Meta</div>
+    return <>
+    <UserSignIn/>
+    </>
 };
 
 ReactDOM.render(<App/>, document.querySelector("#app"));
