0f6615a5ab2886ad72d92d283e18364716e5cd08
Eva Despinoy <despinoy@kth.se>
2021-10-01 11:40:15 +0200

    solve merge conflict

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -4,19 +4,16 @@ import { Provider } from 'react-redux';
 import store from './redux/store'
 import { AddBeverageToMenuPresenter } from './components/presenters/addBeverageToMenuPresenter';
 import UserSignIn from './components/views/userSignIn';
-<<<<<<< HEAD
-=======
 import DrinkModel from './model/drinkModel';
 
 const drinkModel = new DrinkModel(); 
->>>>>>> 7afb8ab (Both APIs working together)
 
 const App = () => {
     return (
         <Provider store={store}>
             <div>
                 <div>Hello Meta</div>
-                <AddBeverageToMenuPresenter />
+                <AddBeverageToMenuPresenter drinkModel={drinkModel}/>
                 <UserSignIn/>
             </div>  
         </Provider>
