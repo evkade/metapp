16108f73de007293809a8bb7addfcf470b3a54c5
Amalia <aberglof@kth.se>
2021-10-13 13:42:54 +0200

    initial layout done

--- client/src/components/views/userSignIn.tsx
+++ client/src/components/views/userSignIn.tsx
@@ -1,40 +1,76 @@
-import React from 'react'; 
-import { useState } from 'react';
-import '../components.css';
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "../components.css";
+import Fingerprint from "../images/fingerprint.png";
 
-const UserSignIn = ({userAuth, checkUserAuth}) => {
-    const [username, setUsername] = useState('');
-    const [pwd, setPwd] = useState('');
+const UserSignIn = ({ userAuth, checkUserAuth, signInError }) => {
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
 
-    return (
+  return (
     <>
-    <div className="signInForm">
-        {!userAuth ?
-        <>
+      <div className="signInForm brickBackground">
+        {!userAuth && (
+          <>
             <div id="form-login" className="signInForm__form">
-                <h1>Login</h1>
-                <input type="text" id="signInUsr" name="username" className="signInForm__form__input" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)} ></input>
-                <input type="password" id="signInPwd" name="password" className="signInForm__form__input" placeholder="Password" value={pwd} onChange={(event) => setPwd(event.target.value)} ></input>
-                <button className="signInForm__form__submit"
-                    onClick={() => {
-                        checkUserAuth(username, pwd)}
-                    }>
-                    Submit</button>
-            </div>
-            <div className="signInForm__errElements">
-                <div id="usr-pwdError" className="signInForm__errElement">
-                    There is no user with these credentials.
+              <div className="signInForm__form--flexRow">
+                <div className="signInForm__logo">
+                  <img src={Fingerprint} />
                 </div>
+                <div className="signInForm__title">Log in</div>
+              </div>
+              <div className="signInForm__inputContainer">
+                <input
+                  type="text"
+                  id="signInUsr"
+                  name="username"
+                  className="signInForm__inputContainer--input"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                ></input>
+                <input
+                  type="password"
+                  id="signInPwd"
+                  name="password"
+                  className="signInForm__form__input"
+                  placeholder="Password"
+                  value={pwd}
+                  onChange={(event) => setPwd(event.target.value)}
+                ></input>
+                <button
+                  id="signInForm__form__submit"
+                  className="signInForm__form__submit"
+                  onClick={() => {
+                    checkUserAuth(username, pwd);
+                  }}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
-        </>
-        :
-        <div className="signInForm--success">
-            <div id="signInForm__successElement" className="signInForm__successElement">
-                    You're successfully logged in!
+            {signInError && (
+              <div className="signInForm__errElements">
+                <div id="usr-pwdError" className="signInForm__errElement">
+                  🚫 There is no user with these credentials.
+                </div>
+              </div>
+            )}
+          </>
+        )}
+        {userAuth && (
+          <div className="signInForm--success">
+            <div
+              id="signInForm__successElement"
+              className="signInForm__successElement"
+            >
+              You're successfully logged in!
             </div>
-        </div>}
-    </div>
-    </>)
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default UserSignIn;
