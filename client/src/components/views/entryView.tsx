3cbc96cc452a2a4168f7cc65aad23ec4140345cb
Amalia <aberglof@kth.se>
2021-10-14 11:56:29 +0200

    scss structure added

--- client/src/components/views/entryView.tsx
+++ client/src/components/views/entryView.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import "../components.css";
+import "../components.scss";
 import mkmlogo from "../images/mkm_logo.png";
 import dkmlogo from "../images/dkm_logo.png";
 
--- client/src/components/views/userSignIn.tsx
+++ client/src/components/views/userSignIn.tsx
@@ -1,40 +1,63 @@
-import React from 'react'; 
-import { useState } from 'react';
-import '../components.css';
+import React from "react";
+import { useState } from "react";
 
-const UserSignIn = ({userAuth, checkUserAuth}) => {
-    const [username, setUsername] = useState('');
-    const [pwd, setPwd] = useState('');
+const UserSignIn = ({ userAuth, checkUserAuth }) => {
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
 
-    return (
+  return (
     <>
-    <div className="signInForm">
-        {!userAuth ?
-        <>
+      <div className="signInForm">
+        {!userAuth ? (
+          <>
             <div id="form-login" className="signInForm__form">
-                <h1>Login</h1>
-                <input type="text" id="signInUsr" name="username" className="signInForm__form__input" placeholder="Username" value={username} onChange={(event) => setUsername(event.target.value)} ></input>
-                <input type="password" id="signInPwd" name="password" className="signInForm__form__input" placeholder="Password" value={pwd} onChange={(event) => setPwd(event.target.value)} ></input>
-                <button className="signInForm__form__submit"
-                    onClick={() => {
-                        checkUserAuth(username, pwd)}
-                    }>
-                    Submit</button>
+              <h1>Login</h1>
+              <input
+                type="text"
+                id="signInUsr"
+                name="username"
+                className="signInForm__form__input"
+                placeholder="Username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              ></input>
+              <input
+                type="password"
+                id="signInPwd"
+                name="password"
+                className="signInForm__form__input"
+                placeholder="Password"
+                value={pwd}
+                onChange={(event) => setPwd(event.target.value)}
+              ></input>
+              <button
+                className="signInForm__form__submit"
+                onClick={() => {
+                  checkUserAuth(username, pwd);
+                }}
+              >
+                Submit
+              </button>
             </div>
             <div className="signInForm__errElements">
-                <div id="usr-pwdError" className="signInForm__errElement">
-                    There is no user with these credentials.
-                </div>
+              <div id="usr-pwdError" className="signInForm__errElement">
+                There is no user with these credentials.
+              </div>
             </div>
-        </>
-        :
-        <div className="signInForm--success">
-            <div id="signInForm__successElement" className="signInForm__successElement">
-                    You're successfully logged in!
+          </>
+        ) : (
+          <div className="signInForm--success">
+            <div
+              id="signInForm__successElement"
+              className="signInForm__successElement"
+            >
+              You're successfully logged in!
             </div>
-        </div>}
-    </div>
-    </>)
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default UserSignIn;
