5ca0d1500e60fbe39bbbf8aff90d5fabf2cdf46b
Amalia <aberglof@kth.se>
2021-10-18 09:33:48 +0200

    changed back to switch states

--- client/src/redux/reducers/user.jsx
+++ client/src/redux/reducers/user.jsx
@@ -5,21 +5,24 @@ const initialState = {
 };
 
 const userReducer = (state = initialState, action) => {
-  if (action.type == "SIGN_IN")
-    return {
-      ...state,
-      loggedIn: true,
-      username: action.payload.username,
-      isAdmin: action.payload.isAdmin,
-    };
-  if (action.type == "SIGN_OUT")
-    return {
-      ...state,
-      loggedIn: false,
-      username: null,
-      isAdmin: false,
-    };
-  else return state;
+  switch (action.type) {
+    case "SIGN_IN":
+      return {
+        ...state,
+        loggedIn: true,
+        username: action.payload.username,
+        isAdmin: action.payload.isAdmin,
+      };
+    case "SIGN_OUT":
+      return {
+        ...state,
+        loggedIn: false,
+        username: null,
+        isAdmin: false,
+      };
+    default:
+      return state;
+  }
 };
 
 export default userReducer;
