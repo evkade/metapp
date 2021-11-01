2d24b792281efa83ab5742a7d96ab9de37cbc38e
Eva Despinoy <despinoy@kth.se>
2021-11-01 10:20:44 +0100

    add textarea to ingredients

--- client/src/components/views/newBeverageForm.tsx
+++ client/src/components/views/newBeverageForm.tsx
@@ -78,9 +78,8 @@ export const NewBeverageForm = ({
         <div>
           <div className="beverage-form__row">
             Ingredients:
-            <input
-              className="beverage-form__input"
-              type="text"
+            <textarea
+              className="beverage-form__input beverage-form__textarea"
               name="ingredients"
               value={newBeverage.ingredients.join(", ")}
               onChange={(e) =>
